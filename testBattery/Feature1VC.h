c858210617899de62263eefcd62431d33614c932
CalinChitu <calin.chitu@softvision.ro>
2015-01-27 14:50:57 +0200

    Add a new class and push to master.

---
+++ testBattery/Feature1VC.h
@@ -0,0 +1,13 @@
+//
+//  Feature1VC.h
+//  testBattery
+//
+//  Created by Calin Chitu on 1/27/15.
+//  Copyright (c) 2015 Calin Chitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Feature1VC : UIViewController
+
+@end
---
+++ testBattery/Feature1VC.m
@@ -0,0 +1,13 @@
+//
+//  Feature1VC.m
+//  testBattery
+//
+//  Created by Calin Chitu on 1/27/15.
+//  Copyright (c) 2015 Calin Chitu. All rights reserved.
+//
+
+#import "Feature1VC.h"
+
+@implementation Feature1VC
+
+@end

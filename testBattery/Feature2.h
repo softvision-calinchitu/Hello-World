1a408279b8d7c16c8e3b3966617b3b72c88bb3bc
Calin Chitu <calin.chitu@softvision.ro>
2015-01-27 15:31:42 +0200

    Feature 2

---
+++ testBattery/Feature2.h
@@ -0,0 +1,13 @@
+//
+//  Feature2.h
+//  testBattery
+//
+//  Created by Calin Chitu on 1/27/15.
+//  Copyright (c) 2015 Calin Chitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Feature2 : UIViewController
+
+@end

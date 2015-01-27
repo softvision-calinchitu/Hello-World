46f2edac5722286780ef7160d3c3ffa604a931e7
CalinChitu <calin.chitu@softvision.ro>
2015-01-27 13:08:36 +0200

    Test

--- testBattery/ViewController.m
+++ testBattery/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *batteryLabel;
+
 @end
 
 @implementation ViewController
@@ -17,13 +19,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    UIDevice *device = [UIDevice currentDevice];
+    device.batteryMonitoringEnabled = YES;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryLevelDidChange:) name:UIDeviceBatteryLevelDidChangeNotification object:device];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryLevelDidChange:) name:UIDeviceBatteryStateDidChangeNotification object:device];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self batteryLevelDidChange:nil];
+}
+
+- (void)viewDidUnload
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self setBatteryLabel:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
+- (void)batteryLevelDidChange:(NSNotification *)notification
+{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    [numberFormatter setNumberStyle:NSNumberFormatterPercentStyle];
+    [numberFormatter setMaximumFractionDigits:1];
+    
+    UIDeviceBatteryState batteryState = [[UIDevice currentDevice] batteryState];
+    if (batteryState == UIDeviceBatteryStateUnplugged) {
+        self.batteryLabel.text = [NSString stringWithFormat:@"Battery: %.0f%% [unplugged]",
+                                  [[UIDevice currentDevice] batteryLevel]*100];
+    } else {
+        self.batteryLabel.text = [NSString stringWithFormat:@"Battery: %.0f%% [charging]",
+                                  [[UIDevice currentDevice] batteryLevel]*100];
+    }
 }
 
 @end

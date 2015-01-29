ce7752ed454b3f7beb1faec62f7d14d9951245fe
Calin Chitu <calin.chitu@softvision.ro>
2015-01-29 15:46:32 +0200

    Kiwi integration.

--- testBatteryTests/testBatteryTests.m
+++ testBatteryTests/testBatteryTests.m
@@ -1,34 +1,21 @@
 //
-//  testBatteryTests.m
-//  testBatteryTests
+//  SatelliteTests.m
+//  SatelliteTests
 //
-//  Created by Calin Chitu on 9/4/14.
-//  Copyright (c) 2014 Calin Chitu. All rights reserved.
+//  Created by Mihai Georgescu on 28.01.2015.
+//
 //
 
-#import <XCTest/XCTest.h>
+#import <Kiwi/Kiwi.h>
 
-@interface testBatteryTests : XCTestCase
+SPEC_BEGIN(MathSpec)
 
-@end
+describe(@"Math", ^{
+    it(@"is pretty cool", ^{
+        NSUInteger a = 16;
+        NSUInteger b = 26;
+        [[theValue(a + b) should] equal:theValue(42)];
+    });
+});
 
-@implementation testBatteryTests
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
-@end
+SPEC_END
